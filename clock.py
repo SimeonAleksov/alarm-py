67a01f2c640b1fd252e1d46a9893038a3103da21
SimeonAleksov <aleksov_s@outlook.com>
2017-03-12 21:35:26 +0100

    song is played at a given time

--- clock.py
+++ clock.py
@@ -3,9 +3,18 @@
 from time import sleep
 from os import system
 from datetime import datetime
+from pygame import mixer
+
 
 def clear():
     system("clear")
+
+ #loading a song to play
+
+def play_song(path="Traumer - Hoodlum DESOLAT038.mp3"):
+    mixer.init()
+    mixer.music.load(path)
+    mixer.music.play()
 
 def show_clock():
     #we need update our time no
@@ -15,8 +24,9 @@ def show_clock():
             clear()
             print current_time.hour, ":",current_time.minute,":",current_time.second
             # just testing when a time hits certain seconds to print a message
-            if current_time.second == 40:
+            if current_time.second == 5:
                 print "ITS TIME WAKE UP"
+                play_song()
             sleep(0.2)
         #stop
         except KeyboardInterrupt:
