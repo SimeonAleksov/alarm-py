7ce1a0fd2f85d072c51175e3a00d38ffae5ee90e
SimeonAleksov <aleksov_s@outlook.com>
2017-03-14 13:39:38 +0100

    fixed and added to manually set an alarm but still some bugs

--- clock.py
+++ clock.py
@@ -9,11 +9,10 @@ from pygame import mixer
 def clear():
     system("clear")
 
-def setup_alarm():
-    user_input = input("Enter when you want to set up alarm (HH : MM): ")
-    alarm = [time for time in user_input.split() if time.isdigit()]
-    return alarm[0], alarm[1]
+user_input = input("Enter when you want to set up alarm (HH : MM): ")
+alarm = [int(time) for time in user_input.split() if time.isdigit()]
 
+hour, minute = alarm[0], alarm[1]
 
 
  # loading a song to play
@@ -24,23 +23,21 @@ def play_song(path="Traumer - Hoodlum DESOLAT038.mp3"):
     mixer.music.load(path)
     mixer.music.play()
 
-# hour, minute = setup_alarm()
+
 
 def show_clock():
-    hour, minute = setup_alarm()
     # we need update our time no
     while True:
         try:
-            print(hour, minute)
             current_time = datetime.now()
             clear()
             print (current_time.hour, ":", current_time.minute, ":", current_time.second)
-            # just testing when a time hits certain seconds to print a message
+            print("Alarm set up in {} : {} : 0".format(hour, minute))
 
-            if current_time.hour == hour and current_time.minute == minute:
-                print(hour, minute)
-                print ("ITS TIME WAKE UP")
+            if current_time.hour == hour and current_time.minute == minute and current_time.second == 0:
+                print("alarm")
                 play_song()
+
             sleep(0.2)
         # stop
         except KeyboardInterrupt:
